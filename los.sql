02d206e28bc5ab3bcba8c5d53d616147faf0ee89
chunhung <kakahow@gmail.com>
2020-12-12 16:12:01 +0800

    cohort will filter in those with mechanical ventilator icu

--- los.sql
+++ los.sql
@@ -3,19 +3,18 @@
 
 
 
+select 
 
-select
-
-icu_patient.gender,
-icu_patient.anchor_age,
-icu_patient.dod,
-admittime,
-dischtime,
-deathtime,
-admission_type,
-admission_location,
-discharge_location,
-insurance,edregtime,
+adm_patient.gender,
+adm_patient.anchor_age,
+adm_patient.dod,
+admittime, 
+dischtime, 
+deathtime, 
+admission_type, 
+admission_location, 
+discharge_location, 
+insurance,edregtime, 
 edouttime,
 hospital_expire_flag,
 first_icu.*,
@@ -27,13 +26,13 @@ from
 (
 	select *
 		from mimic_core.admissions adm
-	join
+	join 
 		mimic_core.patients pat
 	on pat.subject_id = adm.subject_id
-	where pat.anchor_age > 15
-	and adm.hospital_expire_flag = 0
+	where pat.anchor_age > 18
+	and adm.hospital_expire_flag = 0 
 
-  ) icu_patient
+  ) adm_patient
 
 
 join
@@ -42,20 +41,32 @@ join
 
 
 (
-
+	  
 
 	select icu.*
 		from mimic_icu.icustays icu
 	join
 	(
-		select hadm_id  , min(intime) as min_intime
-		FROM
-			mimic_icu.icustays
-		GROUP BY hadm_id
+		select hadm_id  , min(intime) as min_intime 
+		FROM 
+			mimic_icu.icustays 
+		GROUP BY hadm_id 
 	) first_
 	on icu.hadm_id = first_.hadm_id and icu.intime = first_.min_intime
-
+	  
 ) first_icu
-on icu_patient.hadm_id = first_icu.hadm_id
+on adm_patient.hadm_id = first_icu.hadm_id
 
+
+join
+
+(
+
+	select stay_id 
+	from chartevents 
+	where
+		lower(value) in ('drager','avea','pb 7200','sensor medic (hfo)','hamilton')
+) uses_mech_vent
+
+on first_icu.stay_id = uses_mech_vent.stay_id
 
